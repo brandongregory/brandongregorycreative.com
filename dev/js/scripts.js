f80935864658cca0c629b81359dc6f6fca62d75a
brandongregory <brandon.gregory.1981@gmail.com>
2019-05-26 19:05:14 -0500

    A few accessibility fixes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,6 +2,9 @@ const navbarToggle = document.querySelector(".navbar-toggle");
 const navbarNav = document.querySelector("nav .nav");
 
 navbarToggle.addEventListener("click", function(clickEvent) {
+    const currentAriaVal = navbarToggle.getAttribute("aria-collapsed");
+    const newAriaVal = (currentAriaVal === "true") ? "false" : "true";
     navbarToggle.classList.toggle("collapsed");
+    navbarToggle.setAttribute("aria-label", newAriaVal);
     navbarNav.classList.toggle("open");
 });
--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,2 +1,2 @@
 
-const navbarToggle=document.querySelector(".navbar-toggle");const navbarNav=document.querySelector("nav .nav");navbarToggle.addEventListener("click",function(clickEvent){navbarToggle.classList.toggle("collapsed");navbarNav.classList.toggle("open");});
+const navbarToggle=document.querySelector(".navbar-toggle");const navbarNav=document.querySelector("nav .nav");navbarToggle.addEventListener("click",function(clickEvent){const currentAriaVal=navbarToggle.getAttribute("aria-collapsed");const newAriaVal=(currentAriaVal==="true")?"false":"true";navbarToggle.classList.toggle("collapsed");navbarToggle.setAttribute("aria-label",newAriaVal);navbarNav.classList.toggle("open");});
